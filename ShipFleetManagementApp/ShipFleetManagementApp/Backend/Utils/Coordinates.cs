7d78dcb9c6dfc9948a0dd10b6599c6e35fc2f672
Alicja Wagner <alicja.wagn@gmail.com>
2024-05-06 23:43:03 +0200

    added Coordinates and LocationTimestamp classes

---
+++ ShipFleetManagementApp/ShipFleetManagementApp/Backend/Utils/Coordinates.cs
@@ -0,0 +1,31 @@
+﻿namespace ShipFleetManagementApp.Backend.Utils
+{
+    public struct Coordinates
+    {
+        public Coordinates(double latitude, double longitude)
+        {
+            if (latitude >= -90.0 && latitude <= 90.0)
+            {
+                Latitude = latitude;
+            }
+            else
+            {
+                throw new ArgumentException("Latitude is incorrect. It should be in range [-90, 90].");
+            }
+
+            if (longitude >= -180.0 && longitude < 180.0)
+            {
+                Longitude = longitude;
+            }
+            else
+            {
+                throw new ArgumentException("Longitude is incorrect. It should be in range [-180, 180).");
+            }
+        }
+
+        public double Latitude { get; }
+        public double Longitude { get; }
+
+        public override string ToString() => $"Coordinates: ({Latitude}, {Longitude})";
+    }
+}
---
+++ ShipFleetManagementApp/ShipFleetManagementApp/Backend/Utils/LocationTimestamp.cs
@@ -0,0 +1,14 @@
+﻿namespace ShipFleetManagementApp.Backend.Utils
+{
+    public struct LocationTimestamp
+    {
+        public LocationTimestamp(Coordinates position)
+        {
+            Position = position;
+            Time = DateTime.Now;
+        }
+
+        public Coordinates Position { get; }
+        public DateTime Time { get; }
+    }
+}
