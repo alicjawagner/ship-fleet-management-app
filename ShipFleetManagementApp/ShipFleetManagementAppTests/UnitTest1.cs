cc211c156ebe3fd679dd08a5dd76684249664d46
Alicja Wagner <alicja.wagn@gmail.com>
2024-05-06 18:10:23 +0200

    initial project structure

---
+++ ShipFleetManagementApp/ShipFleetManagementAppTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ShipFleetManagementAppTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
