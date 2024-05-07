139ae696bd0250badcd281a32705503f968e0635
Alicja Wagner <alicja.wagn@gmail.com>
2024-05-07 22:35:57 +0200

    done Fuel class and tests for it

---
+++ ShipFleetManagementApp/ShipFleetManagementApp/Backend/Ships/Cargo/Fuel.cs
@@ -0,0 +1,45 @@
+﻿namespace ShipFleetManagementApp.Backend.Ships.Cargo
+{
+    public class Fuel
+    {
+        private static Fuel? _dieselFuel = null;
+        private static Fuel? _heavyFuel = null;
+
+        private Fuel(string type, double density)
+        {
+            Type = type;
+            Density = density;
+        }
+
+        public string Type { get; }
+        /// <summary>
+        /// Density in kg/liter.
+        /// </summary>
+        public double Density { get; }
+
+        public static Fuel Diesel
+        {
+            get
+            {
+                if (_dieselFuel == null)
+                {
+                    _dieselFuel = new Fuel("Diesel", 0.85);
+                }
+                return _dieselFuel;
+            }
+        }
+
+        public static Fuel HeavyFuel
+        {
+            get
+            {
+                if (_heavyFuel == null)
+                {
+                    _heavyFuel = new Fuel("Heavy Fuel", 0.95);
+                }
+                return _heavyFuel;
+            }
+        }
+    }
+
+}
---
+++ ShipFleetManagementApp/ShipFleetManagementAppTests/Backend/Ships/Cargo/FuelTests.cs
@@ -0,0 +1,27 @@
+﻿using ShipFleetManagementApp.Backend.Ships.Cargo;
+using System.Numerics;
+
+namespace ShipFleetManagementAppTests.Backend.Ships.Cargo
+{
+    [TestFixture]
+    public class FuelTests
+    {
+        [Test]
+        public void Diesel_GotDieselInstance()
+        {
+            Fuel diesel = Fuel.Diesel;
+
+            Assert.That(diesel.Type, Is.EqualTo("Diesel"));
+            Assert.That(diesel.Density, Is.EqualTo(0.85));
+        }
+
+        [Test]
+        public void HeavyFuel_GotHeavyFuellInstance()
+        {
+            Fuel heavyFuel = Fuel.HeavyFuel;
+
+            Assert.That(heavyFuel.Type, Is.EqualTo("Heavy Fuel"));
+            Assert.That(heavyFuel.Density, Is.EqualTo(0.95));
+        }
+    }
+}
