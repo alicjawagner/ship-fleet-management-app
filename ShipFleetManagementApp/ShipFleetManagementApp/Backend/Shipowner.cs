6fb2f6c9c9c25783bc115b4a95a9dfb8925ffff1
Alicja Wagner <alicja.wagn@gmail.com>
2024-05-08 15:52:14 +0200

    done Shipowner and ShipownersManager classes

--- ShipFleetManagementApp/ShipFleetManagementApp/Backend/Shipowner.cs
+++ ShipFleetManagementApp/ShipFleetManagementApp/Backend/Shipowner.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShipFleetManagementApp.Backend.Ships;
 
 namespace ShipFleetManagementApp.Backend
 {
     public class Shipowner
     {
+        public Shipowner(string name)
+        {
+            Name = name;
+        }
+
+        public string Name {  get; }
+        public List<Ship> Ships { get; } = [];
+
+        /// <summary>
+        /// Adds a ship of any type to the shipowner's ship list.
+        /// </summary>
+        /// <param name="ship"></param>
+        public void AddShip(Ship ship)
+        {
+            Ships.Add(ship);
+        }
+
+        /// <summary>
+        /// Prints all ships of the shipowner.
+        /// </summary>
+        public void PrintShips()
+        {
+            for (int i = 0; i < Ships.Count; i++)
+            {
+                Console.WriteLine($"{i}: {Ships[i]}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
 }
--- ShipFleetManagementApp/ShipFleetManagementApp/Backend/ShipownersManager.cs
+++ ShipFleetManagementApp/ShipFleetManagementApp/Backend/ShipownersManager.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShipFleetManagementApp.Backend
+﻿namespace ShipFleetManagementApp.Backend
 {
     public class ShipownersManager
     {
+        public ShipownersManager()
+        {
+        }
+
+        public List<Shipowner> Shipowners { get; } = [];
+        
+        /// <summary>
+        /// Registers the new shipowner.
+        /// </summary>
+        /// <param name="ship"></param>
+        public void AddShipowner(Shipowner shipowner)
+        {
+            Shipowners.Add(shipowner);
+        }
+
+        /// <summary>
+        /// Prints all shipowners.
+        /// </summary>
+        public void PrintShipowners()
+        {
+            for (int i = 0; i < Shipowners.Count; i++)
+            {
+                Console.WriteLine($"{i}: {Shipowners[i]}");
+            }
+        }
     }
 }
