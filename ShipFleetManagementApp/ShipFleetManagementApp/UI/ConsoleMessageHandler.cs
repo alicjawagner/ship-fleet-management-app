1bc234bb650e8c50dd7fdf5b2cb1d88eb0e992a7
Alicja Wagner <alicja.wagn@gmail.com>
2024-05-11 18:57:16 +0200

    changed the order of methods in UI

--- ShipFleetManagementApp/ShipFleetManagementApp/UI/ConsoleMessageHandler.cs
+++ ShipFleetManagementApp/ShipFleetManagementApp/UI/ConsoleMessageHandler.cs
@@ -13,9 +13,7 @@ namespace ShipFleetManagementApp.UI
         private static Ship? _currentShip = null;
         private static ContainerShip? _currentContainerShip = null;
         private static TankerShip? _currentTankerShip = null;
-        /// <summary>
-        /// true - it is a container ship, false - it is a tanker ship
-        /// </summary>
+        /// <summary>true - it is a container ship, false - it is a tanker ship</summary>
         private static bool _isContainerShip = false;
         private static Tank? _currentTank = null;
         
@@ -52,6 +50,68 @@ namespace ShipFleetManagementApp.UI
         }
 
         /// <summary>
+        /// Shows welcome message. Should only be used once, at the beginning.
+        /// </summary>
+        public static void ShowWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Ship Fleet Management Application!\n");
+            Console.WriteLine("Here you can manage your fleet, add shipowners and ships.\n");
+        }
+
+        /// <summary>
+        /// Shows the message when the user wants to exit the program.
+        /// </summary>
+        public static void ShowExitMessage()
+        {
+            Console.WriteLine("\nYou are now exiting the application. Thank you for using it, and see you soon!");
+        }
+
+        /// <summary>
+        /// Shows the initial menu allowing to add/select/show a shipowner.
+        /// </summary>
+        public static void ShowInitialSelectionMenu()
+        {
+            _currentShipowner = null;
+
+            ShowMenuHeading();
+            Console.WriteLine(
+                "1. Add a shipowner.\n" +
+                "2. Choose a shipowner.\n" +
+                "3. Show shipowners.");
+            ShowExitOption();
+        }
+
+        /// <summary>
+        /// Performs a given task related to adding/choosing/showing shipowners.
+        /// </summary>
+        /// <param name="input"></param>
+        public static void ReactToInitialMenuChoice(int input)
+        {
+            switch (input)
+            {
+                case 1: // add shipowner
+                    AddShipowner();
+                    break;
+
+                case 2: // choose shipowner
+                    int shipownerIndex = ChooseShipowner();
+                    if (shipownerIndex == -1) { break; }
+                    ShowShipownersMenu();
+                    int choice = ReadIntInput();
+                    ReactToShipownersMenuChoice(choice);
+                    break;
+
+                case 3: // print shipowners
+                    _shipownersManager.PrintShipowners();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input. Please enter a valid option.");
+                    break;
+            }
+        }
+
+        /// <summary>
         /// Reads user's input and casts it to double.
         /// </summary>
         /// <returns>The chosen number (double).</returns>
@@ -79,15 +139,6 @@ namespace ShipFleetManagementApp.UI
         }
 
         /// <summary>
-        /// Shows welcome message. Should only be used once, at the beginning.
-        /// </summary>
-        public static void ShowWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to the Ship Fleet Management Application!\n");
-            Console.WriteLine("Here you can manage your fleet, add shipowners and ships.\n");
-        }
-
-        /// <summary>
         /// Shows the heading for each menu (Choose what would you like to do)
         /// </summary>
         private static void ShowMenuHeading()
@@ -101,21 +152,6 @@ namespace ShipFleetManagementApp.UI
         private static void ShowExitOption()
         {
             Console.WriteLine("-1. Exit the program.\n");
-        }
-
-        /// <summary>
-        /// Shows the initial menu allowing to add/select/show a shipowner.
-        /// </summary>
-        public static void ShowInitialSelectionMenu()
-        {
-            _currentShipowner = null;
-
-            ShowMenuHeading();
-            Console.WriteLine(
-                "1. Add a shipowner.\n" +
-                "2. Choose a shipowner.\n" +
-                "3. Show shipowners.");
-            ShowExitOption();
         }
 
         /// <summary>
@@ -135,6 +171,19 @@ namespace ShipFleetManagementApp.UI
                 "1. Add a ship to the shipowner's list.\n" +
                 "2. Choose a ship from the shipowner's list.\n" +
                 "3. Show the shipowner's ships.");
+            ShowExitOption();
+        }
+
+        /// <summary>
+        /// Shows the menu allowing to choose the type of the ship to be added.
+        /// </summary>
+        private static void ShowShipAddingMenu()
+        {
+            Console.WriteLine("\nAdding a new ship to the shipowner's list.\n" +
+                "Choose the type of the ship:\n" +
+                "0. Go back.\n" +
+                "1. Container ship.\n" +
+                "2. Tanker ship.");
             ShowExitOption();
         }
 
@@ -171,6 +220,20 @@ namespace ShipFleetManagementApp.UI
         }
 
         /// <summary>
+        /// Show the menu for specifying the configuration of tanks on this ship.
+        /// </summary>
+        private static void ShowTanksAddingMenu()
+        {
+            Console.WriteLine("\nPlease specify the configuration of tanks for this ship.");
+            Console.WriteLine("You can add tanks one by one or add several tanks of the same type at once. Choose what would you like to do:");
+            Console.WriteLine(
+                "1. Add a tank.\n" +
+                "2. Add several tanks at once.\n" +
+                "3. Finish adding tanks.");
+            ShowExitOption();
+        }
+
+        /// <summary>
         /// Shows the menu allowing to refuel/empty the tank.
         /// </summary>
         private static void ShowTanksMenu()
@@ -185,33 +248,30 @@ namespace ShipFleetManagementApp.UI
         }
 
         /// <summary>
-        /// Performs a given task related to adding/choosing/showing shipowners.
+        /// Shows the menu for choosing the value to be changed (during creating a ship).
         /// </summary>
-        /// <param name="input"></param>
-        public static void ReactToInitialMenuChoice(int input)
+        private static void ShowShipValuesChangeMenu()
         {
-            switch (input)
-            {
-                case 1: // add shipowner
-                    AddShipowner();
-                    break;
+            Console.WriteLine("Please choose the number of the value you'd like to change.");
+            Console.WriteLine(
+                "1. IMO Number\n" +
+                "2. Name\n" +
+                "3. Length\n" +
+                "4. Width\n" +
+                "5. Latitude\n" +
+                "6. Longitude\n" +
+                "7. Maximum load");
+        }
 
-                case 2: // choose shipowner
-                    int shipownerIndex = ChooseShipowner();
-                    if (shipownerIndex == -1) { break; }
-                    ShowShipownersMenu();
-                    int choice = ReadIntInput();
-                    ReactToShipownersMenuChoice(choice);
-                    break;
-
-                case 3: // print shipowners
-                    _shipownersManager.PrintShipowners();
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid input. Please enter a valid option.");
-                    break;
-            }
+        /// <summary>
+        /// Shows the menu for choosing the value to be changed (during updating the ship position).
+        /// </summary>
+        private static void ShowPositionUpdateValuesChangeMenu()
+        {
+            Console.WriteLine("Please choose the number of the value you'd like to change.");
+            Console.WriteLine(
+                "1. Latitude\n" +
+                "2. Longitude");
         }
 
         /// <summary>
@@ -319,6 +379,10 @@ namespace ShipFleetManagementApp.UI
             }
         }
 
+        /// <summary>
+        /// Performs a given task: refueling/emptying tank.
+        /// </summary>
+        /// <param name="input"></param>
         private static void ReactToTanksMenuChoice(int input)
         {
             bool stop = false;
@@ -349,6 +413,87 @@ namespace ShipFleetManagementApp.UI
                 {
                     ShowTanksMenu();
                     input = ReadIntInput();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs adding a new shipowner.
+        /// </summary>
+        private static void AddShipowner()
+        {
+            Console.WriteLine("\nAdding a new shipowner.\nEnter the shipowner's name:");
+
+            string? name = Console.ReadLine();
+            while (name == null)
+            {
+                Console.WriteLine("Enter the shipowner's name:");
+                name = Console.ReadLine();
+            }
+            _shipownersManager.AddShipowner(new Shipowner(name));
+            Console.WriteLine($"Shipowner named {name} has been added successfully.");
+        }
+
+        /// <summary>
+        /// Performs choosing a shipowner from the list.
+        /// </summary>
+        /// <returns>The index of the chosen shipowner or -1 if no shipowner chosen.</returns>
+        private static int ChooseShipowner()
+        {
+            if (_shipownersManager.Shipowners.Count == 0)
+            {
+                Console.WriteLine("There are no shipowners to choose from yet. First add a shipowner.");
+                return -1;
+            }
+
+            Console.WriteLine("\nChoosing a shipowner.\nEnter the shipowner's number:");
+            _shipownersManager.PrintShipowners();
+            int choice = ReadIntInput();
+
+            while (!_shipownersManager.IsShipownerIndexValid(choice))
+            {
+                Console.WriteLine("There's no such shipowner.\nEnter the shipowner's number:");
+                _shipownersManager.PrintShipowners();
+                choice = ReadIntInput();
+            }
+
+            _currentShipowner = _shipownersManager.Shipowners[choice];
+            return choice;
+        }
+
+        /// <summary>
+        /// Performs the process of selection the ship type.
+        /// </summary>
+        private static void AddShip()
+        {
+            ShowShipAddingMenu();
+            int choice = ReadIntInput();
+            bool stop = false;
+
+            while (!stop)
+            {
+                switch (choice)
+                {
+                    case 0: //go back
+                        stop = true;
+                        break;
+                    case 1: // add container ship
+                        AddContainerShip();
+                        choice = 0;
+                        break;
+                    case 2: // add tanker ship
+                        AddTankerShip();
+                        choice = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter a valid option.");
+                        break;
+                }
+
+                if (choice != 0) // user doesn't go back, they stay in the menu
+                {
+                    ShowShipAddingMenu();
+                    choice = ReadIntInput();
                 }
             }
         }
@@ -388,99 +533,6 @@ namespace ShipFleetManagementApp.UI
                 _currentTankerShip = (TankerShip)_currentShip;
             }
             return choice;
-        }
-
-        /// <summary>
-        /// Performs choosing a shipowner from the list.
-        /// </summary>
-        /// <returns>The index of the chosen shipowner or -1 if no shipowner chosen.</returns>
-        private static int ChooseShipowner()
-        {
-            if (_shipownersManager.Shipowners.Count == 0)
-            {
-                Console.WriteLine("There are no shipowners to choose from yet. First add a shipowner.");
-                return -1;
-            }
-
-            Console.WriteLine("\nChoosing a shipowner.\nEnter the shipowner's number:");
-            _shipownersManager.PrintShipowners();
-            int choice = ReadIntInput();
-
-            while (!_shipownersManager.IsShipownerIndexValid(choice))
-            {
-                Console.WriteLine("There's no such shipowner.\nEnter the shipowner's number:");
-                _shipownersManager.PrintShipowners();
-                choice = ReadIntInput();
-            }
-
-            _currentShipowner = _shipownersManager.Shipowners[choice];
-            return choice;
-        }
-
-        /// <summary>
-        /// Performs adding a new shipowner.
-        /// </summary>
-        private static void AddShipowner()
-        {
-            Console.WriteLine("\nAdding a new shipowner.\nEnter the shipowner's name:");
-
-            string? name = Console.ReadLine();
-            while (name == null) {
-                Console.WriteLine("Enter the shipowner's name:");
-                name = Console.ReadLine();
-            }
-            _shipownersManager.AddShipowner(new Shipowner(name));
-            Console.WriteLine($"Shipowner named {name} has been added successfully.");
-        }
-
-        /// <summary>
-        /// Shows the menu allowing to choose the type of the ship to be added.
-        /// </summary>
-        private static void ShowShipAddingMenu()
-        {
-            Console.WriteLine("\nAdding a new ship to the shipowner's list.\n" +
-                "Choose the type of the ship:\n" +
-                "0. Go back.\n" +
-                "1. Container ship.\n" +
-                "2. Tanker ship.");
-            ShowExitOption();
-        }
-
-        /// <summary>
-        /// Performs the process of selection the ship type.
-        /// </summary>
-        private static void AddShip()
-        {
-            ShowShipAddingMenu();
-            int choice = ReadIntInput();
-            bool stop = false;
-
-            while (!stop)
-            {
-                switch (choice)
-                {
-                    case 0: //go back
-                        stop = true;
-                        break;
-                    case 1: // add container ship
-                        AddContainerShip();
-                        choice = 0;
-                        break;
-                    case 2: // add tanker ship
-                        AddTankerShip();
-                        choice = 0;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input. Please enter a valid option.");
-                        break;
-                }
-
-                if (choice != 0) // user doesn't go back, they stay in the menu
-                {
-                    ShowShipAddingMenu();
-                    choice = ReadIntInput();
-                }
-            }
         }
 
         /// <summary>
@@ -939,55 +991,6 @@ namespace ShipFleetManagementApp.UI
             {
                 Console.WriteLine(e.Message);
             }
-        }
-
-        /// <summary>
-        /// Show the menu for specifying the configuration of tanks on this ship.
-        /// </summary>
-        private static void ShowTanksAddingMenu()
-        {
-            Console.WriteLine("\nPlease specify the configuration of tanks for this ship.");
-            Console.WriteLine("You can add tanks one by one or add several tanks of the same type at once. Choose what would you like to do:");
-            Console.WriteLine(
-                "1. Add a tank.\n" +
-                "2. Add several tanks at once.\n" +
-                "3. Finish adding tanks.");
-            ShowExitOption();
-        }
-
-        /// <summary>
-        /// Shows the menu for choosing the value to be changed (during creating a ship).
-        /// </summary>
-        private static void ShowShipValuesChangeMenu()
-        {
-            Console.WriteLine("Please choose the number of the value you'd like to change.");
-            Console.WriteLine(
-                "1. IMO Number\n" +
-                "2. Name\n" +
-                "3. Length\n" +
-                "4. Width\n" +
-                "5. Latitude\n" +
-                "6. Longitude\n" +
-                "7. Maximum load");
-        }
-
-        /// <summary>
-        /// Shows the menu for choosing the value to be changed (during updating the ship position).
-        /// </summary>
-        private static void ShowPositionUpdateValuesChangeMenu()
-        {
-            Console.WriteLine("Please choose the number of the value you'd like to change.");
-            Console.WriteLine(
-                "1. Latitude\n" +
-                "2. Longitude");
-        }
-
-        /// <summary>
-        /// Shows the message when the user wants to exit the program.
-        /// </summary>
-        public static void ShowExitMessage()
-        {
-            Console.WriteLine("\nYou are now exiting the application. Thank you for using it, and see you soon!");
         }
     }
 }
